6d823e16e07f04a5136f4a82a49aa8f9c4786760
`Jose Manuel Chincho` <`chinchocardosa@gmail.com`>
2024-07-15 19:35:08 +0200

    configure the android app in firebase distribution

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'es.oppenheimer.app',
+    appId: 'es.test.jmchincho',
     appName: 'poc-capacitor-app-react',
     webDir: 'dist',
     "plugins": {
